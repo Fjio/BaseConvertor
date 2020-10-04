cd8d8e173d38de7fc68998397dd6f0c20a3b486f
Pierre Bourreau <pibourreau@laposte.net>
2020-10-04 12:22:49 +0200

    Both sides converter done

--- convertor.py
+++ convertor.py
@@ -6,6 +6,7 @@ def baseToDec(baseString:str, base:int) -> int:
   (1-9 A-Z so base < 37)
   Get your decimal number in return
   """
+
   num=0
   dictToBe = {}
   helperString=list(range(10))+list(string.ascii_uppercase)
@@ -19,5 +20,28 @@ def baseToDec(baseString:str, base:int) -> int:
 
   return(num)
 
+def decToBase(decNum:int, base:int) -> int:
+  """
+  Pass a written integer in base 10
+  Pass the numeral system radix | counting base
+  (1-9 A-Z so base < 37)
+  Get your number in the base you chose in return
+  """
+  strBase = ""
+  dictToBe = {}
+  helperString=list(range(10))+list(string.ascii_uppercase)
+
+  for i in range(base):
+    ch=helperString[i]
+    dictToBe[i] = ch
+
+  q = decNum
+  while q!=0:
+    r = q%base
+    q//=base
+    strBase = str(dictToBe[r]) + strBase
+
+  return(strBase)
 #examples
-print(baseToDec("A12",16))
+#print(baseToDec("A12",16))
+#print(decToBase(35631,16))
